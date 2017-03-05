49871d4557ab6756d5c5321899b27467e88bc564
Eunice Kim <ekimchicago@hotmail.com>
2017-03-05 15:32:32 -0800

    add feature

--- dietcoke/server.R
+++ dietcoke/server.R
@@ -1,26 +1,37 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
-library(shiny)
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+library("dplyr")
+library('rsconnect')
+library("maps")
+library("mapproj")
+library("plyr")
+library("httr")
+library("jsonlite")
+library("knitr")
+library("ggplot2")
+library("tidyr")
+library("mapdata")
+library("plotly")
+library("stringr")
+crime <- read.csv("crime_dataset.csv", stringsAsFactors = F)
+server <- function(input, output) {
    
-  output$distPlot <- renderPlot({
+  output$plot<- renderPlot({
+    total.crime <- ddply(crime, c("State"), function(x){
+      sum.inc <- sum(x$Incident)
+      data.frame(sum.incident = sum.inc)
+    })
+    total.crime$State <- tolower(total.crime$State)
+    states <- map_data("state")
+    map.df<-merge(states, total.crime, by.x = "region", by.y = "State", all.x=T)
+    map.df <- map.df[order(map.df$order),]
+    map1 <- ggplot(map.df, aes(x=long,y=lat,group=group))+
+      geom_polygon(aes(fill=sum.incident))+
+      geom_path()+ ggtitle("Crime in the US")+
+      scale_fill_gradientn(colours=rev(heat.colors(10)),na.value="grey90")+
+      coord_map()+theme_minimal()
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+   
   })
   
-})
+}
+
+shinyServer(server)

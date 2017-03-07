4ee61e520fc703f80d3d95cf82173ac6a2d7516f
Eunice Kim <ekimchicago@hotmail.com>
2017-03-06 22:28:22 -0800

    add feature

--- dietcoke/server.R
+++ dietcoke/server.R
@@ -11,10 +11,21 @@ library("tidyr")
 library("mapdata")
 library("plotly")
 library("stringr")
+library("shiny")
+library("ggvis")
+
 crime <- read.csv("crime_dataset.csv", stringsAsFactors = F)
+
 server <- function(input, output) {
-   
-  output$plot<- renderPlot({
+    
+    output$plot <- renderPlot({
+      year = input$year
+      crime.plot <- crime %>% select(Perpetrator.Age, Victim.Count, Year) %>% group_by(Perpetrator.Age) %>% summarise(Victim = sum(Victim.Count))
+      ggplot(data = crime.plot[crime.plot$year > input$year[1] & crime.plot$year < input$year[2]], aes(Perpetrator.Age, Victim)) +
+        geom_point(size = input$size)
+    })
+    
+  output$map <- renderPlot({
     total.crime <- ddply(crime, c("State"), function(x){
       sum.inc <- sum(x$Incident)
       data.frame(sum.incident = sum.inc)
@@ -28,10 +39,9 @@ server <- function(input, output) {
       geom_path()+ ggtitle("Crime in the US")+
       scale_fill_gradientn(colours=rev(heat.colors(10)),na.value="grey90")+
       coord_map()+theme_minimal()
-    
-   
+   return(map1)
   })
-  
 }
 
 shinyServer(server)
+

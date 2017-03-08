29f37ceccc15c3bc8f86affe7fe294b48b3598f5
ahree <ariac@uw.edu>
2017-03-08 15:14:53 -0800

    Add files via upload

--- dietcoke/server.R
+++ dietcoke/server.R
@@ -1,30 +1,24 @@
 library("dplyr")
 library('rsconnect')
-library("maps")
-library("mapproj")
-library("plyr")
 library("httr")
 library("jsonlite")
 library("knitr")
 library("ggplot2")
-library("tidyr")
-library("mapdata")
-library("plotly")
-library("stringr")
-crime <- read.csv("data/crime_dataset.csv", stringsAsFactors = F)
+
+#crime <- read.csv("data/crime_dataset.csv", stringsAsFactors = F)
 
 server <- function(input, output) {
   
-  output$hist<- renderPlot({
-  bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  crime.plot <- crime %>% select(Victim.Age) 
+  crime.plot <- crime[crime$Victim.Age < 98, ]
   
-   p <- ggplot(data = crime, aes(x=crime$Victim.Age)) 
-      + geom_histogram(binwidth = bins, color="grey", aes(fill=..count..))
+  output$hist<- renderPlot({
     
-    if(input$mean){p+ geom_vline(aes(xintercept=mean(crime$Victim.Age)),
-                  color="white", linetype="dashed", size=1)
-    }
-    
+    hist(crime.plot$Victim.Age,
+         probability = TRUE,
+         breaks = as.numeric(input$bins),
+         xlab = "Victim Ages",
+         main = "Victim Ages")
   })
 }
 
--- dietcoke/ui.R
+++ dietcoke/ui.R
@@ -3,29 +3,23 @@ library('ggplot2')
 library('rsconnect')
 library('dplyr')
 
-crime <- read.csv("data/crime_dataset.csv", stringsAsFactors = F)
+#crime <- read.csv("data/crime_dataset.csv", stringsAsFactors = F)
 
 ui <- fluidPage(
-
+  
   titlePanel("American Homocide Rates by Race, Gender, and Age"),
   
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30),
-   
-       checkboxInput("mean",
-                   "Add Meanline",
-                   value = TRUE)
-    ),
+      selectInput(inputId = "bins",
+                  label = "Number of bins:",
+                  choices = c(10, 20, 35, 50),
+                  selected = 20)),
     
     mainPanel(
       tabsetPanel(type = "tabs", 
                   tabPanel("Plot"), 
-                  tabPanel("Histogram", fluidRow(plotOutput('hist'))),
+                  tabPanel("Histogram", plotOutput('hist')),
                   tabPanel("Map"),
                   tabPanel("Pie Chart"))
     )

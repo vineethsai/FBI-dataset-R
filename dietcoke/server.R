ef6b333569914f8545a748c3f2e43bc3e2e26ba0
Eunice Kim <ekimchicago@hotmail.com>
2017-03-08 19:26:11 -0800

    add feature

--- dietcoke/server.R
+++ dietcoke/server.R
@@ -19,7 +19,6 @@ crime <- read.csv("crime_dataset.csv", stringsAsFactors = F)
 server <- function(input, output) {
   
   crime.plot <- crime %>% select(Perpetrator.Age, Incident) %>% filter(Perpetrator.Age != 0) %>% group_by(Perpetrator.Age) %>% summarise(Count = sum(Incident))
- 
    output$plot <- renderPlot({
       ggplot(data = crime.plot, aes(Perpetrator.Age, Count)) +
         geom_point(size = input$size)
@@ -49,6 +48,43 @@ server <- function(input, output) {
       coord_map()+theme_minimal()
    return(map1)
   })
+  
+  bar.data <- crime %>% select(Victim.Race, Year, Incident) %>% filter(Victim.Race != "Unknown")
+  
+  selected <- reactive({
+    selected <- input$n
+    bar.data <- crime[crime$Year == selected, ]
+  })
+  
+  output$info <- renderText({
+    str2 <- paste0(c("During ", input$n), collapse = "")
+    str1 <- paste0(" there were ", nrow(crime[crime$Victim.Race == "Black", ]), " black victims, ")
+    str3 <- paste0(nrow(crime[crime$Victim.Race == "White", ]), " white victims, ")
+    str4 <- paste0(nrow(crime[crime$Victim.Race == "Native American/Alaska Native", ]), " Native American or Alaska Native victims, ")
+    str5 <- paste0(" and ", nrow(crime[crime$Victim.Race == "Asian/Pacific Islander", ]), " Asian/Pacific Islander victims.")
+    paste0(str2, str1, str3, str4, str5)
+  })
+  
+  # Victim Race
+  output$plot1 <- renderPlot({
+    plot1.data <- selected()
+    plot1 <- ggplot(plot1.data) + geom_bar(aes(x = factor(""), fill = Victim.Race)) + coord_polar(theta = "y") + scale_x_discrete("") +
+      ggtitle("Victim Race by Year") + labs(fill = "Victim Race")
+    print(plot1)
+  })
+  
+  crime.hist <- crime %>% select(Victim.Age) 
+  crime.hist <- crime[crime$Victim.Age < 98, ]
+  
+  output$hist<- renderPlot({
+    
+    hist(crime.hist$Victim.Age,
+         probability = TRUE,
+         breaks = as.numeric(input$bins),
+         xlab = "Victim Ages",
+         main = "Victim Ages")
+  })
+
 }
 
 shinyServer(server)
